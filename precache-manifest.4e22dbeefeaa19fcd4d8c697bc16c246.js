199ea3146d3614e6941ad57968abbf041e87aaf7
Ismail Ulas Bayram <iulasbayram@gmail.com>
2020-05-30 18:23:55 +0300

    Updates

---
+++ precache-manifest.4e22dbeefeaa19fcd4d8c697bc16c246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a34004a31081575fe0bf845900e8e0c",
+    "url": "/react-landing-page/index.html"
+  },
+  {
+    "revision": "9c2a4a483676fbd515df",
+    "url": "/react-landing-page/static/css/main.ca741f0f.chunk.css"
+  },
+  {
+    "revision": "af91ea71be9470497ca5",
+    "url": "/react-landing-page/static/js/2.3e7d9847.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-landing-page/static/js/2.3e7d9847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2a4a483676fbd515df",
+    "url": "/react-landing-page/static/js/main.0eac7f55.chunk.js"
+  },
+  {
+    "revision": "cbab37d442e0bf226809",
+    "url": "/react-landing-page/static/js/runtime-main.9880db9c.js"
+  },
+  {
+    "revision": "b6e118c7095ac83285e1488f21794845",
+    "url": "/react-landing-page/static/media/jellyfish.b6e118c7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66585ffad30edb7ff37f0a663903a7d6.js"
+  "/react-landing-page/precache-manifest.4e22dbeefeaa19fcd4d8c697bc16c246.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-landing-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
