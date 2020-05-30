48793e1b6d0862789117a326f58940e632b4a79a
Ismail Ulas Bayram <iulasbayram@gmail.com>
2020-05-30 19:23:09 +0300

    CodeStyle Refactoring

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -69,7 +69,7 @@ const styles = {
 
 const Experience = () => <VerticalTimelineComp />;
 
-const Schedule = ({ events = eventsList }) => (
+const Schedule = () => (
   <section {...styles.container}>
     <Experience />
   </section>
